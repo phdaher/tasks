411634e9749b50eefaebe05cef6c4ff6d372b9e6
Pedro Daher <pedrohenriquedaher@icloud.com>
2020-12-01 22:30:36 -0300

    Adds get and create tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tasks', views.getTasks, name='tasks'),
+    path('tasks/create', views.createTask, name='create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import JsonResponse, HttpResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+def getTasks(request):
+    tasks = Task.objects.all()
+    task_serializer = TaskSerializer(tasks, many=True)
+    return JsonResponse(task_serializer.data, safe=False)
+
+
+def createTask(request):
+    task_data = JSONParser().parse(request)
+    task_serializer = TaskSerializer(data=task_data)
+    if task_serializer.is_valid():
+        task_serializer.save()
+        return JsonResponse(task_serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(task_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
